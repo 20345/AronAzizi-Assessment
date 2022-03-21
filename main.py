b7a4008664f3dd5bf49b9032380319f613c03e5f
Aron A <100541124+20345@users.noreply.github.com>
2022-03-21 21:33:48 +0000

    Added Error Handling for Name Input

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-name = input("What is your name? ")
 
-print("""Hello {}, Welcome to this General Knowledge Quiz, """.format(name))
+while True:
+  try:
+    name = input("What Is Your Name? ")
+    if name.isalpha():
+      break
+  except ValueError:
+    print("Invalid Name ")
+
+
+print("""Hello {}, Welcome to this General Knowledge Quiz,
+you will go through a variety of 
+10-20 questions ranged from easy to difficult as you progress.""".format(name))
+print(" ")
+
+print("Good luck and I hope you enjoy {}!".format(name))
