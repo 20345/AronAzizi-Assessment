00e9af98560cf814ebe1dd875ef4821763ba947a
Aron A <100541124+20345@users.noreply.github.com>
2022-04-08 01:00:08 +0000

    Added comments which indicate what each part or system of code does

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#Name input system system with error handling
 while True:
   try:
     name = input("What is your name? ")
@@ -6,9 +7,10 @@ while True:
   except ValueError:
     print("Please enter a valid name ")
 
+#Input of instructions and steps for the quiz
 print("""Hello {}, welcome to this general knowledge quiz,
 you will go through a variety of 
-10-20 questions ranged from easy to difficult as you progress. \n
+5-15 questions ranged from easy to difficult as you progress. \n
 You will receive 1 point for every question you get right! 
 These points will be added up and will display your final score
 at the end of the quiz.
@@ -16,6 +18,7 @@ This will determine your level of intelligent
 in your knowledge of General Knowledge""".format(name))
 print("   ")
 
+#Age input system with error handling
 while True:
       try:
           age = input("Before you begin, please enter your age ")
@@ -25,15 +28,15 @@ while True:
           print("Invalid age")
 
 
-
+#Final message before starting the quiz
 print("Good luck and I hope you enjoy {}!".format(name))
 
 print (" ")
 
+#Quetions collection
+questions = { 'Question 1: Machu Picchu is located in which country? \n a: Brazil \n b: Peru \n c: Costa Rica \n d: Philippines \n \n' : 'b' , 'Question 2: What is the biggest bone in the body? \n a: Femur \n b: Tibula \n c: Pectoralis Major  \n d: Ulna \n' :  'a' , 'Question 3: What is the largest muscle in the body? \n a: Gastrocnemius \n b: Latissimus Dorsi \n c: Brachioradialis \n d: Gluteus Maximus \n' : 'd' , 'Question 4: What is the biggest mammal to ever live? \n a: Titanosaur \n b: Argentinosaurus \n c: Mammoth \n d: Blue Whale \n' : 'd' , 'Question 5: What is the smallest planet in our solar system? \n a: Venus \n b: Mars \n c: Jupiter \n d: Neptune \n e: Saturn \n f: Mercury \n g: Earth \n h: Uranus \n' : 'f' , 'Question 6:'}
 
-questions = { 'Question 1: Machu Picchu is located in which country? \n a: Brazil \n b: Peru \n c: Costa Rica \n d: Philippines \n \n' : 'b' , 'Question 2: What is the biggest bone in the body? \n a: Femur \n b: Tibula \n c: Pectoralis Major  \n d: Ulna \n' :  'a' , 'Question 3: What is the largest muscle in the body? \n a: Gastrocnemius \n b: Latissimus Dorsi \n c: Brachioradialis \n d: Gluteus Maximus \n' : 'd' , 'Question 4: What is the biggest mammal to ever live? \n a: Titanosaur \n b: Argentinosaurus \n c: Mammoth \n d: Blue Whale \n' : 'd'}
-
-
+#Score system with accurate precision
 score = 0
 for key in questions.keys():
     user_answer=input(key)
